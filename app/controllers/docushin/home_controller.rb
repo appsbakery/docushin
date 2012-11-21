094ee11f9242d75904eea7c53cd523b4658787c8
Todor Grudev <tagrudev@gmail.com>
2012-11-21 14:13:20 +0200

    removing emacs temp files

--- app/controllers/docushin/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class Docushin::HomeController < ActionController::Base
-  layout 'docushin'
-
-  def index
-    @index = File.read(File.join(Rails.root, "doc", "docushin", "index.md")) rescue nil
-  end
-end
